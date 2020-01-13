8d173c082ce2831b2566b2119be530953a894e7c
Edvin Norling <edvin.norling@gmail.com>
2020-01-13 23:19:52 +0100

    Using a test array instead of just one test
    This to easier scale the number of tests.
    Also add a test for error
    
    Probably should add an os.Exit(int) to the appllication if unable
    to read the folder.

--- jsonparser/jsonparser_test.go
+++ jsonparser/jsonparser_test.go
@@ -4,12 +4,32 @@ import (
 	"testing"
 )
 
-func TestReadDir(t *testing.T) {
-	/* Make test better don't want to fail if i add a file that have a file
-	with a letter earlier then s
-	*/
-	myfiles, _ := ReadDir("testdata")
-	if myfiles[0] != "small_svc.json" {
-		t.Errorf("File is not in folder %v", myfiles[0])
+type IndexDirTest struct {
+	testdir     string
+	expectedOut []string
+}
+
+var myIndex = []IndexDirTest{
+	IndexDirTest{"testdata", []string{"small_svc.json"}},
+	IndexDirTest{"ThisDirDontExist", []string{""}},
+}
+
+func TestReadDirReal(t *testing.T) {
+	// The second test works since err handels it and dosen't create any actual since it's Nil
+	for _, tests := range myIndex {
+		actualOutput, _ := ReadDir(tests.testdir)
+		for i := 0; i < len(actualOutput); i++ {
+			if actualOutput[i] != tests.expectedOut[i] {
+				t.Errorf("The expected files from: %v didn't matche: %v from folder: %v", tests.expectedOut[i], actualOutput[i], tests.testdir)
+			}
+		}
+	}
+}
+
+func TestReadDirError(t *testing.T) {
+	// Test the error case to make sure we get an error.
+	_, err := ReadDir("FolderDontExist")
+	if err == nil {
+		t.Errorf("Crap %v", err)
 	}
 }
